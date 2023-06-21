1b071a054c23f81b2d8fb245e94352c0d5091792
Nicholas M Goldstein <98927571+nicholasmgoldstein@users.noreply.github.com>
2023-06-21 19:34:14 -0400

    Delete errors.go

--- errors.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "errors"
-
-var (
-	ErrNotFound         = errors.New("not found")
-	ErrUnauthorized     = errors.New("unauthorized")
-	ErrInternalServerError = errors.New("internal server error")
-)
